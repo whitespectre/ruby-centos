0bdcd63b04d8441e956fc60cc845f746664a17ff
Timur Batyrshin <erthad@gmail.com>
2016-06-03 18:13:38 +0300

    different package releases for different RH versions

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -2,6 +2,8 @@
 
 VERSION="$(cat "$(dirname $0)/../VERSION")"
 BASEDIR="/opt/ruby-${VERSION}"
+RH_RELEASE="rpm -q --queryformat '%{VERSION}' redhat-release"
+[ -z "${RH_RELEASE}" ] && RH_RELEASE="rpm -q --queryformat '%{VERSION}' centos-release" ||:
 
 # We need FPM to build Ruby and don't want to have it inside the package
 sudo rm -rf /tmp/ruby
@@ -13,4 +15,4 @@ sudo ln -s "${BASEDIR}/bin/ruby" /usr/local/bin/ # required for find-deps to wor
 DEPS="$($(dirname $0)/find-deps /tmp/ruby )"
 
 rm -f ruby${VERSION}-*.rpm
-"${BASEDIR}/bin/fpm" -s dir -t rpm -C /tmp/ruby -n "ruby${VERSION}" --prefix "${BASEDIR}" --version 1 ${DEPS}
+"${BASEDIR}/bin/fpm" -s dir -t rpm -C /tmp/ruby -n "ruby${VERSION}" --prefix "${BASEDIR}" --version "1.0" --iteration "el${RH_RELEASE}" ${DEPS}
