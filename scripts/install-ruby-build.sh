11b2055cd3caad43376eda7846b973cefb761310
Timur Batyrshin <erthad@gmail.com>
2016-06-03 18:58:17 +0300

    support standalone mode

--- scripts/install-ruby-build.sh
+++ scripts/install-ruby-build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 sudo yum install -y git gcc bzip2 openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel xz-devel rpm-build
+
+cd "${HOME}"
 [ -d ruby-build ] || git clone https://github.com/rbenv/ruby-build.git
